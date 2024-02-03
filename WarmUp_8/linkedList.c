d95717b69a6b1fc268648e7ff2c53c10777b09d6
Nicholas Tilelli <pressurebell@proton.me>
2024-02-03 06:37:19 +0000

    Temp linkedlist solution

--- WarmUp_8/linkedList.c
+++ WarmUp_8/linkedList.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct node 
-{
+typedef struct node{
     int data;
     struct node* next;
+}node;
+
+void push(node ** head, int new)
+{
+    struct node * current =  head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    //gcurrent->next = (node *) malloc(sizeof(node));
+    current->next->data = new;
+    current->next->next = NULL;
 }
-void push(node * head, int new)
+void print_list(node * head)
 {
     node * current = head;
     while (current != NULL)
     {
-        current = current -> next;
+        printf("\n %d", current->data);
+        current = current->next;
     }
-    current -> next = (node *) malloc(sizeOf(node));
-    current -> next -> data = data;
-    current -> next -> next = NULL;
 }
-
 int main()
 {
+    node * temp_node = NULL;
+    push(temp_node,5);
+    print_list(temp_node);
 
 }
