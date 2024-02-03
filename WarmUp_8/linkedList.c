ae3fd64e45a2187528e0568eae33a019e8377be3
Nicholas Tilelli <pressurebell@proton.me>
2024-02-03 06:38:14 +0000

    Warmup 8 - Segmented error

--- WarmUp_8/linkedList.c
+++ WarmUp_8/linkedList.c
@@ -1,34 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
+typedef struct node 
+{
     int data;
     struct node* next;
-}node;
-
+} node;
 void push(node ** head, int new)
 {
-    struct node * current =  head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-    //gcurrent->next = (node *) malloc(sizeof(node));
-    current->next->data = new;
-    current->next->next = NULL;
-}
-void print_list(node * head)
-{
-    node * current = head;
+    node * current = * head;
     while (current != NULL)
     {
-        printf("\n %d", current->data);
+        current = current -> next;
+    }
+    //current -> next = (node *) malloc(sizeOf(node));
+    current -> next -> data = new;
+    current -> next -> next = NULL;
+}
+
+int delete(node ** head, int key)
+{
+    node ** current = head;
+    node * temp_node = NULL;
+    int index = -1;
+    int i = 0;
+
+    while (current != NULL && current->data != key) {
+        current = current->next;
+        i++;
+    }
+    if (current->data == key)
+    {
+        temp_node = current->next;
+        current->next = temp_node->next;
+        free(temp_node);
+        index = i;
+    }
+    return index;
+}
+void print_list(node ** head) {
+    node ** current = head;
+    
+    while (current != NULL) {
+        printf("%d\n", current->data);
         current = current->next;
     }
+    
 }
+
 int main()
 {
     node * temp_node = NULL;
-    push(temp_node,5);
-    print_list(temp_node);
-
+    push(&temp_node, 5);
+    push(&temp_node, 3);
+    push(&temp_node, 4);
+    push(&temp_node, 7);
+    push(&temp_node, 8);
+    print_list(&temp_node);
+    printf("\n 7 deleted at index: %d", delete(&temp_node, 7));
 }
+
+node * current = head;
+    while (current->next !=NULL)
+    {
+        current = current->next;
+    }
+    node * new_node = (* node) malloc(sizeof(node));
+    current->next->data = new;
+    current->next->next = NULL;
